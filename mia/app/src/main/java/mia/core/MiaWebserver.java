7cde987607ece00e400361d721450e59a5f0a549
CookieJ4R <cookiej4r@web.de>
2021-05-20 20:50:21 +0200

    Changed Port to 80

--- mia/app/src/main/java/mia/core/MiaWebserver.java
+++ mia/app/src/main/java/mia/core/MiaWebserver.java
@@ -5,7 +5,7 @@ import io.javalin.Javalin;
 public class MiaWebserver {
 
     public MiaWebserver(){
-        Javalin server = Javalin.create().start("192.168.2.101", 8080);
+        Javalin server = Javalin.create().start("192.168.2.101", 80);
         server.get("/test", (ctx) -> {
             ctx.result("Test running");
         });
