1e07445f61b4a4f567c3ddb449b0faa6a2a26813
CookieJ4R <cookiej4r@web.de>
2021-05-25 21:17:24 +0200

    Added MQTTHandler and its Javadoc

---
+++ mia/app/src/main/java/mia/core/MiaMQTTHandler.java
@@ -0,0 +1,71 @@
+package mia.core;
+
+import org.eclipse.paho.client.mqttv3.*;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+/***
+ * This class handles everything MQTT related.
+ * The handler listens on every topic and will emit a command on the CommandBus when a message gets received
+ * These emits will have the following form: "mqtt:TOPIC" and will have the received message split at " " as data
+ * Modules can publish to the mqtt broker via 'publish()'
+ */
+public class MiaMQTTHandler {
+
+    private final static MiaMQTTHandler instance = new MiaMQTTHandler().init();
+    public static MiaMQTTHandler getInstance(){return  instance;}
+
+
+    private MqttClient miaClient;
+
+    /***
+     * Initializes the MqttHandler
+     * @return the MQTTHandler Instance to allow initialization during singleton creation
+     */
+    private MiaMQTTHandler init(){
+        try {
+            String broker = "tcp://192.168.2.128";
+            String clientId = "MiaMqttClient";
+            miaClient = new MqttClient(broker, clientId);
+            System.out.println("Connecting to broker: "+ broker);
+            miaClient.connect();
+            System.out.println("Connected");
+            miaClient.subscribe("#");
+            miaClient.setCallback(new MqttCallback() {
+                @Override
+                public void connectionLost(Throwable cause) {
+                    System.out.println("Lost connection to broker");
+                }
+
+                @Override
+                public void messageArrived(String topic, MqttMessage message){
+                    System.out.println("MQTT Message received");
+                    MiaCommandBus.getInstance().emit("mqtt:" + topic, Arrays.stream(message.toString().trim().split(" ")).toList());
+                }
+
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken token) {
+                    System.out.println("Successfully published message");
+                }
+            });
+        } catch(MqttException me) {
+            me.printStackTrace();
+        }
+        return this;
+    }
+
+    /***
+     * Publish a message to the connected mqtt broker
+     * @param topic the topic to publish to
+     * @param message the message to publish
+     */
+    void publish(String topic, String message){
+        try {
+            miaClient.publish(topic, new MqttMessage(message.getBytes(StandardCharsets.UTF_8)));
+        }catch(MqttException e){
+            e.printStackTrace();
+        }
+    }
+
+}
